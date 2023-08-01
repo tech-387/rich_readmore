7a5d26bf97e6c31b34ac3c7569ca611aa0a2db84
Thierry Oliveira <thierry.oliveira@superformula.com>
2023-08-01 09:59:14 -0300

    test: add tests for uncovered lines

--- test/src/presentation/rich_readmore_test.dart
+++ test/src/presentation/rich_readmore_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rich_readmore/rich_readmore.dart';
@@ -5,54 +6,107 @@ import 'package:rich_readmore/rich_readmore.dart';
 import '../../mocks/rich_readmore_mocks.dart';
 
 void main() {
-  group('Using TextSpan', () {
-    group('with LineModeSettings', () {
-      final settings = LineModeSettings(
-        trimLines: 3,
-        colorClickableText: Colors.pink,
-        trimCollapsedText: 'Expand',
-        trimExpandedText: ' Collapse ',
+  Widget buildWidget(ReadMoreSettings settings) => MaterialApp(
+        home: RichReadMoreText(RichReadMoreMocks.textSpan, settings: settings),
       );
+  group('with LineModeSettings', () {
+    final settings = LineModeSettings(
+      trimLines: 3,
+      colorClickableText: Colors.pink,
+      trimCollapsedText: 'Expand',
+      trimExpandedText: ' Collapse ',
+    );
 
-      Widget buildWidget(ReadMoreSettings settings) => MaterialApp(
-            home: RichReadMoreText(RichReadMoreMocks.textSpan,
-                settings: settings),
-          );
+    testWidgets('should displays correct text', (WidgetTester tester) async {
+      final widget = buildWidget(settings);
+      await tester.pumpWidget(widget);
 
-      testWidgets('should displays correct text', (WidgetTester tester) async {
-        final widget = buildWidget(settings);
-        await tester.pumpWidget(widget);
-
-        final textFinder = find.text('Don\'t have an account? Sign up');
-        expect(textFinder, findsOneWidget);
-      });
+      final textFinder = find.text('Don\'t have an account? Sign up');
+      expect(textFinder, findsOneWidget);
     });
 
-    group('with LengthModeSettings', () {
-      final settings = LengthModeSettings(
-        trimLength: 10,
+    testWidgets('should displays correct text from string',
+        (WidgetTester tester) async {
+      final widget = MaterialApp(
+        home: RichReadMoreText.fromString(
+            text: 'Don\'t have an account? Sign up', settings: settings),
+      );
+      await tester.pumpWidget(widget);
+
+      final textFinder = find.text('Don\'t have an account? Sign up');
+      expect(textFinder, findsOneWidget);
+    });
+
+    testWidgets('should ', (WidgetTester tester) async {
+      final widget = buildWidget(LineModeSettings(
+        trimLines: 3,
         colorClickableText: Colors.pink,
-        trimCollapsedText: 'Expand',
-        trimExpandedText: ' Collapse ',
+        trimCollapsedText:
+            'Just a COLLAPSED test with a really long action text with characters enough to wrap line and does not have space enough being the action text greater than the max width!',
+        trimExpandedText:
+            'Just a EXPANDED test with a really long action text with characters enough to wrap line and does not have space enough being the action text greater than the max width!',
+      ));
+      await tester.pumpWidget(widget);
+
+      final textFinder = find.text('Don\'t have an account? Sign up');
+      expect(textFinder, findsOneWidget);
+    });
+  });
+
+  group('with LengthModeSettings', () {
+    final settings = LengthModeSettings(
+      trimLength: 10,
+      colorClickableText: Colors.pink,
+      trimCollapsedText: 'Expand',
+      trimExpandedText: ' Collapse ',
+    );
+
+    testWidgets(
+        'should displays the expand action text when greater than the provided length for trimming',
+        (WidgetTester tester) async {
+      final widget = buildWidget(settings);
+      await tester.pumpWidget(widget);
+
+      final richText =
+          find.byType(RichText).evaluate().single.widget as RichText;
+      final plainText = richText.text.toPlainText();
+
+      expect(plainText.contains(settings.trimCollapsedText), isTrue);
+    });
+  });
+
+  group('common tests', () {
+    final settings = LengthModeSettings(
+      trimLength: 10,
+      colorClickableText: Colors.pink,
+      trimCollapsedText: 'Expand',
+      trimExpandedText: ' Collapse ',
+    );
+    testWidgets('should update the action text after pressed',
+        (WidgetTester tester) async {
+      final widget = buildWidget(settings);
+      await tester.pumpWidget(widget);
+
+      final richText = tester.widget<RichText>(find.byType(RichText));
+
+      richText.text.visitChildren(
+        (visitor) {
+          if (visitor is TextSpan &&
+              visitor.text?.trim() == settings.trimCollapsedText.trim()) {
+            (visitor.recognizer as TapGestureRecognizer).onTap?.call();
+
+            return false;
+          }
+          return true;
+        },
       );
 
-      Widget buildWidget(ReadMoreSettings settings) => MaterialApp(
-            home: RichReadMoreText(RichReadMoreMocks.textSpan,
-                settings: settings),
-          );
+      await tester.pumpAndSettle();
+      final updatedRichText =
+          find.byType(RichText).evaluate().single.widget as RichText;
+      final plainText = updatedRichText.text.toPlainText();
 
-      testWidgets(
-          'should displays the expand action text when greater than the provided length for trimming',
-          (WidgetTester tester) async {
-        final widget = buildWidget(settings);
-        await tester.pumpWidget(widget);
-
-        final richText =
-            find.byType(RichText).evaluate().single.widget as RichText;
-        final plainText = richText.text.toPlainText();
-
-        expect(plainText.contains(settings.trimCollapsedText), isTrue);
-      });
+      expect(plainText.contains(settings.trimExpandedText), isTrue);
     });
   });
 }
